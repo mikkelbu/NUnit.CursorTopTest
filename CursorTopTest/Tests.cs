3bb4953bd0a09d0ec93f76dd68afd9ac2a48c03f
Mikkel Nylander Bundgaard <mikkelbu@gmail.com>
2019-04-21 22:17:33 +0200

    Add minimal repo of CursorTop

---
+++ CursorTopTest/Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CursorTopTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestWithCursorTop()
+        {
+            Assert.That(Console.CursorTop, Is.EqualTo(0));
+        }
+    }
+}
